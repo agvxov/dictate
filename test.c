1eb047a15efd5760803dc16392d240eadd620ac8
anon <anon@anon.anon>
2025-02-15 02:37:43 +0100

    add failing test

--- test.c
+++ test.c
@@ -34,6 +34,14 @@ signed main(void) {
     mdictate("$r###$0 ", "this following text\nhas a margin\non the left\n");
 
     dictate("--------\n");
+    mdictate("### ",
+        "There are ", 2, " hard problems in programming:\n",
+        0, ") naming things\n",
+        1, ") cache misses\n",
+        2, ") off by one errors\n"
+    );
+
+    dictate("--------\n");
     dictate("using %s a %x format specifier %d without 'f' does nothing\n");
 
     return 0;
